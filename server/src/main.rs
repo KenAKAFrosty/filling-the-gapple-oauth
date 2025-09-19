42753ab52ff1fa174b111c22c054093633da4b65
KenAKAFrosty <kenakafrosty@gmail.com>
2025-09-19 11:43:44 -0500

    get tracing in eagerly, prefer it over println

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,15 @@
+use tracing_subscriber::{EnvFilter, fmt::format::FmtSpan};
+
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
-    println!("Hello, OAuth!");
+    let _ = dotenvy::dotenv();
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
+        )
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
+
+    tracing::info!("Hello, Oauth!");
 }
