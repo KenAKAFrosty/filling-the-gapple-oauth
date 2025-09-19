0a20e270f87e62722c2cbb0199156037d7805037
KenAKAFrosty <kenakafrosty@gmail.com>
2025-09-19 11:56:40 -0500

    get basic axum server up

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,10 @@
+mod app_state;
+use std::env;
+
+use axum::{Router, routing::get};
+use tower::ServiceBuilder;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
+use tracing::Level;
 use tracing_subscriber::{EnvFilter, fmt::format::FmtSpan};
 
 #[tokio::main(flavor = "multi_thread")]
@@ -10,6 +17,26 @@ async fn main() {
         )
         .with_span_events(FmtSpan::CLOSE)
         .init();
+    tracing::info!("Initializing server");
 
-    tracing::info!("Hello, Oauth!");
+    let port = env::var("PORT").unwrap_or("4242".to_string());
+    let server_address = format!("0.0.0.0:{}", port);
+    tracing::info!("Beginning to listen on {}", server_address);
+    let server_listener = tokio::net::TcpListener::bind(server_address)
+        .await
+        .expect("Unable to bind listener to server_address: {}");
+
+    let server_middleware = ServiceBuilder::new().layer(
+        TraceLayer::new_for_http()
+            .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+            .on_response(DefaultOnResponse::new().level(Level::INFO)),
+    );
+
+    let server_router = Router::new()
+        .route("/healthcheck", get(|| async { "OK" }))
+        .layer(server_middleware);
+
+    axum::serve(server_listener, server_router)
+        .await
+        .expect("Unable to start server");
 }
